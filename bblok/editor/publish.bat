4fb486fcb388ba57453c429e21f1f17f622efcc8
fajar <rokhman.fajar@gmail.com>
2024-08-27 20:52:25 +0700

    bblok

--- bblok/editor/publish.bat
+++ bblok/editor/publish.bat
@@ -1,7 +1,7 @@
 set BB_DEST_DIR=D:\xampp4\htdocs\io\bblok
 
 7z a -tzip bblok ./build/*
-xcopy bblok.7z "G:\My Drive\blockly"
+xcopy bblok.zip "G:\My Drive\blockly"
 xcopy .\build\* %BB_DEST_DIR% /i /s /y
 
 pause
